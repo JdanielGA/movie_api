a9664ee845a70315f56d187ea289a7d9f9186808
Daniel Granada <j.daniel_granada@hotmail.com>
2023-09-02 17:59:32 -0500

    Functions for searching and creating were added

--- config/database.py
+++ config/database.py
@@ -5,16 +5,16 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # The name of the database file.
-sqlite_file_name = 'database.sqlite'
+sqlite_file_name = '../database/database.sqlite'
 
 # The path to the directory containing the database file.
 base_dir = os.path.dirname(os.path.realpath(__file__))
 
-# The database URI.
-database_uri = 'sqlite:///' + os.path.join(base_dir, sqlite_file_name)
+# The database URL.
+database_url = f'sqlite:///{os.path.join(base_dir, sqlite_file_name)}'
 
 # Create an engine to connect to the database.
-engine = create_engine(database_uri, echo=True)
+engine = create_engine(database_url, echo=True)
 
 # Create a session to interact with the database.
 session = sessionmaker(bind=engine)
--- models/movies.py
+++ models/movies.py
@@ -1,11 +1,26 @@
+from config.database import Base
+from sqlalchemy import Column, Integer, String, Float
 from pydantic import BaseModel, Field
 from typing import Optional
 
 
+class MovieModel(Base):
+
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(50))
+    overview = Column(String(500))
+    year = Column(Integer)
+    rating = Column(Float)
+    category = Column(String(50))
+
+
 class Movie(BaseModel):
-    id: Optional[int] = None
-    title: str = Field(default='The movie', min_length=2, max_length=50)
+
+    id: Optional[int]
+    title: str = Field(default='The movie title', max_length=50)
     overview: str = Field(default='The movie overview', min_length=5, max_length=500)
-    year: int = Field(default=2023, ge=1900, le=2100)
+    year: int = Field(default=2023, ge=1900, le=2050)
     rating: float = Field(default=0.0, ge=0.0, le=10.0)
-    category: str = Field(default='Category', min_length=2, max_length=50)
+    category: str = Field(default='The movie category', max_length=50)
